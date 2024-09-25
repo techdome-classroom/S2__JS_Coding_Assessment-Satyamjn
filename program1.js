8d2a1aace7932851abdafad5a77ee889e0f5b96e
Satyam jain <satyamjain0655@gmail.com>
2024-09-25 18:13:15 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -11,8 +11,7 @@ var isValid = function(s) {
             if(!stack.length || (c==')' && stack[stack.length-1]!='(') ||
             (c=='}' && stack[stack.length-1]!='{')|| 
             (c==']' && stack[stack.length-1]!='[')){
-                return true;
-                
+                return true; 
             }
             stack.pop();
         }
