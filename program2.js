d8672b171b606f21d75391a29786fb0ab0ef3972
Satyam jain <satyamjain0655@gmail.com>
2024-09-25 18:27:46 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -8,9 +8,9 @@ var romanToInt = function(s) {
        'V': 5,
         'X':10,
         'L': 50,
-         'C': 100,
-          'D': 500 ,
-          'M':1000
+        'C': 100,
+        'D': 500 ,
+        'M':1000
     }
     let ans=0;
     let prev=0;
